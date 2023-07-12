5a1febc27c171744e1262c87deadcbcb6f19ed16
Samarth <samkusaxena@gmail.com>
2023-07-12 11:04:35 +0530

    changes in hardhat file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,18 @@
+/** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
-
-const { PRIVATE_KEY } = process.env;
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 
 module.exports = {
-  defaultNetwork: "sphinx",
   networks: {
     hardhat: {},
-    sphinx: {
-      url: "https://dapps.shardeum.org/",
+    liberty: {
+      url: "https://liberty20.shardeum.org/",
       chainId: 8081,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [``],
+    },
+    sphinx: {
+      url: "https://sphinx.shardeum.org/",
+      accounts: [``],
     },
   },
-  solidity: {
-    version: "0.8.17",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
-  mocha: {
-    timeout: 40000,
-  },
+  solidity: "0.8.3",
 };
